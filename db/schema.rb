b8ca538b32f2f840b7241cfbc150871090777d0f
Raphael1894 <rapha.pereira.perso@gmail.com>
2021-09-22 13:45:37 +0200

    impost tasks reasy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_101334) do
+ActiveRecord::Schema.define(version: 2021_09_22_105536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2021_09_22_101334) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "rank"
+  end
+
+  create_table "texts", force: :cascade do |t|
+    t.string "word"
+    t.bigint "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "word_frequencies", force: :cascade do |t|
